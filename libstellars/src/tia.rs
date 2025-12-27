ab0fef2b477783e10991973f69c30883a7e30bcf
GamerMine <gamermine@dwightstudio.fr>
2025-12-27 05:44:28 +0100

    A little Wsync to make it (almost) work

--- libstellars/src/tia.rs
+++ libstellars/src/tia.rs
@@ -8,6 +8,7 @@ use crate::tia::colors::NTSC_COLORS;
 pub enum WriteFunctions {
     Vsync = 0x00,
     Vblank = 0x01,
+    Wsync = 0x02,
     Colubk = 0x09,
 }
 #[repr(u8)]
@@ -25,6 +26,7 @@ pub struct Tia {
 
     pic_x: u8,
     pic_y: u8,
+    wsync_enabled: bool,
 }
 
 impl Tia {
@@ -38,12 +40,17 @@ impl Tia {
 
             pic_x: 0x0000,
             pic_y: 0x0000,
+            wsync_enabled: false,
             pic_buffer: [Color { r: 0x00, g: 0x00, b: 0x00 }; SCREEN_WIDTH as usize * SCREEN_HEIGHT as usize],
         }
     }
 
     pub fn set_write_function(&mut self, address: u8, value: u8) {
         self.write_functions[address as usize] = value;
+
+        if address == WriteFunctions::Wsync as u8 {
+            self.wsync_enabled = true;
+        }
     }
 
     pub fn get_write_function(&self, address: WriteFunctions) -> u8 {
@@ -54,26 +61,31 @@ impl Tia {
         for _ in 0..cycles * 3 {
             if (self.get_write_function(WriteFunctions::Vsync) >> 1) & 0x1 == 0x1 ||
                 (self.get_write_function(WriteFunctions::Vblank) >> 1) & 0x1 == 0x1 {
-                self.frame_ready = true;
                 self.pic_x = 0x00;
                 self.pic_y = 0x00;
                 break;
             }
 
-            if self.pic_x >= 228 {
-                self.pic_x = 0;
-                self.pic_y += 1;
-            }
+            loop {
+                if self.pic_x >= 228 {
+                    self.pic_x = 0;
+                    self.pic_y += 1;
+                    self.wsync_enabled = false;
+                }
 
-            if self.pic_y >= 192 {
-                break;
-            }
+                if self.pic_y >= 192 {
+                    self.frame_ready = true;
+                    break;
+                }
 
-            if self.pic_x >= 68 {
-                self.pic_buffer[self.pic_x as usize * self.pic_y as usize] = NTSC_COLORS[self.get_write_function(WriteFunctions::Colubk) as usize];
-            }
+                if self.pic_x >= 68 {
+                    self.pic_buffer[self.pic_y as usize * SCREEN_WIDTH as usize + (self.pic_x as usize - 68)] = NTSC_COLORS[self.get_write_function(WriteFunctions::Colubk) as usize];
+                }
 
-            self.pic_x += 1;
+                self.pic_x += 1;
+
+                if !self.wsync_enabled { break; }
+            }
         }
     }
 }
